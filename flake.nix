22966e5625f71ed74b2cd79e8b2201807bec0d6e
Elisiei Yehorov <yehorovye@disroot.org>
2025-08-19 08:06:23 +0200

    flake

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "minimal flake for go dev";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+
+  outputs =
+    inputs:
+    let
+      goVersion = 24;
+
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ inputs.self.overlays.default ];
+            };
+          }
+        );
+    in
+    {
+      overlays.default = final: prev: {
+        go = final."go_1_${toString goVersion}";
+      };
+
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+            ];
+          };
+        }
+      );
+    };
+}
